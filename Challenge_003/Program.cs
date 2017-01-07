ac08143916e85246d1eb670715d475c61cf6a370
Noah Wilson <ntw9049@g.rit.edu>
2017-01-06 19:30:54 -0500

    Changes a string to a new string one letter at a time printing each iteration of the evolution.

---
+++ Challenge_003/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            Console.Write("Turn: ");
+            string before = Console.ReadLine();
+
+            //output
+            Console.Write("Into: ");
+            string after = Console.ReadLine();
+
+            //will be used to insure the same string is never printed twice in a row
+            string temp = before;
+            
+            //returns if the lengths aren't of equal value in order to prevent errors
+            if(before.Length != after.Length) { Console.WriteLine("Strings must be of equal length"); return; }
+
+            Console.WriteLine(before);
+    
+            for (int i = 0; i < after.Length; i++)
+            {
+                //moves one character up each iteration to change to new string
+                before = after.Substring(0, i + 1) + before.Substring(i + 1, before.Length - i - 1);
+
+                //makes sure it's a new string and not just like the last time through
+                if(temp != before)
+                    Console.WriteLine(before);
+
+                temp = before;
+            }
+        }
+    }
+}
---
+++ Challenge_003/ReadMe.txt
@@ -0,0 +1,17 @@
+Changes one string into a new string one character at a time ex.)
+input: hello there
+output: good bye :)
+hello there
+gello there
+gollo there
+goolo there
+goodo there
+good  there
+good bthere
+good byhere
+good byeere
+good bye re
+good bye :e
+good bye :)
+
+This was a very simple program to make and wasn't much of a challenge to me :( but today I didn't have much time to write code so I wanted to at least build something and at least I knew I would be able to build this in under 30 minutes :)
