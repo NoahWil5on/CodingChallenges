ee56d3b80ca9b5ac99fa9af901c3d8cb30aaba9d
Noah Wilson <ntw9049@g.rit.edu>
2017-01-07 21:53:48 -0500

    For this challenge I created an RPN calculator the takes RPN notation and computes a number, the next step is to have it conversely give you the equation in standard notation.

---
+++ Challenge_004/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_004
+{
+    class Program
+    {
+        static Stack<double> numbers;
+        static string[] pushers;
+        static string[] equation;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Type an expression in RPN with spaces between each number/operator--");
+            string input = Console.ReadLine();
+            pushers = input.Split(' ');
+            equation = input.Split(' ');
+            numbers = new Stack<double>();
+
+            FindResult();
+
+            if(numbers.Count > 1)
+            {
+                Console.WriteLine("The entered string was written with too few operators.");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Pop().ToString());
+            }
+        }
+        protected static void FindResult()
+        {
+            foreach (string s in pushers)
+            {
+                double a;
+                double b;
+
+                if (double.TryParse(s, out a))
+                {
+                    numbers.Push(a);
+                }
+                else {
+                    switch (s)
+                    {
+                        case "+":
+                            b = numbers.Pop();
+                            a = numbers.Pop();
+                            numbers.Push(a + b);
+                            break;
+                        case "-":
+                            b = numbers.Pop();
+                            a = numbers.Pop();
+                            numbers.Push(a - b);
+                            break;
+                        case "*":
+                        case "x":
+                            b = numbers.Pop();
+                            a = numbers.Pop();
+                            numbers.Push(a * b);
+                            break;
+                        case "/":
+                            b = numbers.Pop();
+                            a = numbers.Pop();
+                            numbers.Push(a / b);
+                            break;
+                        case "//":
+                            int c = (int)numbers.Pop();
+                            int d = (int)numbers.Pop();
+                            numbers.Push(d / c);
+                            break;
+                        case "%":
+                            b = numbers.Pop();
+                            a = numbers.Pop();
+                            numbers.Push(a % b);
+                            break;
+                        case "^":
+                            b = numbers.Pop();
+                            a = numbers.Pop();
+                            numbers.Push(Math.Pow(a, b));
+                            break;
+                        case "!":
+                            a = numbers.Pop();
+                            double factorial = 1;
+                            for (int i = 1; i < a + 1; i++)
+                            {
+                                factorial *= i;
+                            }
+                            numbers.Push(factorial);
+                            break;
+                        default:
+                            Console.WriteLine("The entered string contains characters that cannot be parsed.");
+                            return;
+                    }
+                }
+            }
+        }
+    }
+}
