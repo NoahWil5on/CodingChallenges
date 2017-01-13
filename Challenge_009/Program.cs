62be7bad58c0cf9a2f5b8cdf0e35ca3ba0ff58ac
Noah Wilson <ntw9049@g.rit.edu>
2017-01-13 14:57:30 -0500

    Challenge_009 was intended to have me take 2 strings and check to make sure they were 1 or less(0) edits away from being the same string

---
+++ Challenge_009/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            Console.WriteLine(Away(s1,s2).ToString());
+        }
+        /// <summary>
+        /// Take 2 strings and check if they are one or less edit away
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        protected static bool Away(string s1, string s2)
+        {
+            //I used a bit vector because I can easily 
+            //manipulate it to check how many differences
+            //there are
+            int bitVector = 0;
+
+            //Turns on(in some cases turns off) all the used
+            //characters in string 1
+            for(int i = 0; i < s1.Length; i++)
+            {
+                bitVector = Toggle(bitVector, s1[i]);
+            }
+            //Turns off(in some cases turns on) all the used
+            //characters in string 2
+            for (int i = 0; i < s2.Length; i++)
+            {
+                bitVector = Toggle(bitVector, s2[i]);
+            }
+            //if the strings are the same length we are looking
+            //for 2 or 0 differences
+            if (s1.Length == s2.Length)
+            {
+                return (bitVector == 0) || IsDouble(bitVector);
+            }
+            //if the strings are different lengths we are looking
+            //for 1 change
+            else
+            {
+                return IsSingle(bitVector);
+            }
+        }
+        /// <summary>
+        /// Takes a bitVector and then toggles its value
+        /// at index
+        /// </summary>
+        /// <param name="bitVector"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        protected static int Toggle(int bitVector, int index)
+        {
+            int mask = 1 << index-'a';
+            //if different turn bitvector at index on
+            if((bitVector & mask) == 0)
+            {
+                bitVector |= mask;
+            }
+            //if same turn bitvector at index off
+            else
+            {
+                bitVector &= ~mask;
+            }
+            return bitVector;
+        }
+        /// <summary>
+        /// Check for a single change
+        /// </summary>
+        /// <param name="bitVector"></param>
+        /// <returns></returns>
+        protected static bool IsSingle(int bitVector)
+        {
+            return (bitVector & (bitVector - 1)) == 0;
+        }
+        /// <summary>
+        /// Check for 2 changes
+        /// </summary>
+        /// <param name="bitVector"></param>
+        /// <returns></returns>
+        protected static bool IsDouble(int bitVector)
+        {
+            int diff = 0;
+            for(int i = 0; i < 32; i++)
+            {
+                int mask = 1 << i;
+                if((mask & bitVector) > 0)
+                {
+                    diff++;
+                }
+            }
+            if(diff > 2)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Challenge_009/ReadMe.txt
@@ -0,0 +1,29 @@
+For Challenge_009 I took 2 strings and checked if they were 1 (or 0) edit away from being the same string. This is considering an edit to be an insert, removal or replacement of a character to one or both strings.
+
+ex.)
+input: 	pale
+	ple
+output: true
+
+input:	pale
+	pales
+output: true
+
+input:	pale
+	bale
+output:	true
+
+input:	pale
+	bake
+output: false
+
+My approach would take 0(n) time and have the same space complexity as the books solution, however our approaches greatly differed. I noticed that in the case of an insert/removal of a character, there should only be one difference between the characters. So using a bitVector I could simply toggle on and off the position of each character I found in string 1, and then use that same bitVector and toggle on and off each character I found in string 2. Then I could simply check if the bitVectors differed by one bit and return true or false.
+
+In the case that the strings are the same length (perhaps a character had been replaced) I could use the same exact method but instead check for 2 or 0 differences; 2 being a replacement, 0 being the exact same string. This was a fairly straight forward approach, and I was able to implement almost immediatly after writing it down with pen and paper.
+
+
+The book's approach was slightly more, I thought, verbose. The book checked for each case seperatley, and then eventually attempted to check for each case all at once, which became a little more messy and hard to follow. They did so by first considering an insertion and removal of a character the same thing. You could then simply loop through the 2 and check to make sure that (after the misshap of missing character) all character matched perfectly. Similarly, with the case of the replacement of a character, you could just loop through the 2 strings and check to make sure no more than one character was different.
+
+The books solution was very straight forward and made sense. Either solution is works just as well, Maybe the books was easier for someone new to the program to understand. In the end I decided to stick with mine as it worked just as well; I also didn't feel compelled to try and implement the books approach as it was so simple.
+
+-Noah Wilson
